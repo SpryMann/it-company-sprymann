199ae340e9f8076594b02f34399f89e0ecdd6a24
Grigroiy <lyakhovgm@gmail.com>
2020-06-01 21:16:00 +0300

    New update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,34 +1,34 @@
-// import modules
-import smoothscroll from 'smoothscroll-polyfill';
+'use strict';
+
+var _smoothscrollPolyfill = require('smoothscroll-polyfill');
+
+var _smoothscrollPolyfill2 = _interopRequireDefault(_smoothscrollPolyfill);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // link variables
-const nav = document.querySelector('.nav');
-const anchors = document.querySelectorAll('[href^="#"]');
+var nav = document.querySelector('.nav'); // import modules
+
+var anchors = document.querySelectorAll('[href^="#"]');
 
 // create function to underline hovered navigation link
-nav.addEventListener('mouseover', event => {
-    let navLink = event.target.closest('.nav__item-link');
+nav.addEventListener('mouseover', function (event) {
+    var navLink = event.target.closest('.nav__item-link');
 
     if (navLink) {
         navLink.classList.add('active');
     }
-
 });
 
-nav.addEventListener('mouseout', event => {
-    let navLink = event.target.closest('.nav__item-link');
+nav.addEventListener('mouseout', function (event) {
+    var navLink = event.target.closest('.nav__item-link');
 
     if (navLink) {
         navLink.classList.remove('active');
     }
-
 });
 
 // smooth scroll
 (function () {
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-            smoothscroll.polyfill();
-        })
-    }
-}())
+    _smoothscrollPolyfill2.default.polyfill();
+})();
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+// import modules
+import smoothscroll from 'smoothscroll-polyfill';
+
+// link variables
+const nav = document.querySelector('.nav');
+const anchors = document.querySelectorAll('[href^="#"]');
+
+// create function to underline hovered navigation link
+nav.addEventListener('mouseover', event => {
+    let navLink = event.target.closest('.nav__item-link');
+
+    if (navLink) {
+        navLink.classList.add('active');
+    }
+
+});
+
+nav.addEventListener('mouseout', event => {
+    let navLink = event.target.closest('.nav__item-link');
+
+    if (navLink) {
+        navLink.classList.remove('active');
+    }
+
+});
+
+// smooth scroll
+(function () {
+    smoothscroll.polyfill();
+}())
